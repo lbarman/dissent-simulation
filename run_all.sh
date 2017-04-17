6fc233a1e8b85094bab9a26b37391fe959e730d3
Ludovic Barman <ludovic.barman@gmail.com>
2017-04-17 17:14:35 +0200

    Renames config, new executable

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,22 @@
-#!/bin/bash
-rm -f *log
+#!/bin/sh
+
+echo "Cleaning server 0"
+ssh dserver-0.lb-ldd-diss.safer.isi.deterlab.net 'sudo pkill dissent'
+echo "Cleaning server 1"
+ssh dserver-1.lb-ldd-diss.safer.isi.deterlab.net 'sudo pkill dissent'
+echo "Cleaning server 2"
+ssh dserver-2.lb-ldd-diss.safer.isi.deterlab.net 'sudo pkill dissent'
+echo "Cleaning client 0"
+ssh dclient-0.lb-ldd-diss.safer.isi.deterlab.net 'sudo pkill dissent'
+echo "Cleaning client 1"
+ssh dclient-1.lb-ldd-diss.safer.isi.deterlab.net 'sudo pkill dissent'
+echo "Cleaning client 2"
+ssh dclient-2.lb-ldd-diss.safer.isi.deterlab.net 'sudo pkill dissent'
+echo "Cleaning client 3"
+ssh dclient-3.lb-ldd-diss.safer.isi.deterlab.net 'sudo pkill dissent'
+echo "Cleaning client 4"
+ssh dclient-4.lb-ldd-diss.safer.isi.deterlab.net 'sudo pkill dissent'
+
 ./dissent server0.conf &> /dev/null &
 pids=$!
 ./dissent server1.conf &> /dev/null &
