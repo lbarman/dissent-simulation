1510cd37a844421a7ed4e0c9c0624a9cda957434
Ludovic Barman <ludovic.barman@gmail.com>
2017-04-17 19:41:55 +0200

    Automate script

---
+++ automate.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+rm automate.log
+
+for repeat in {0..10}
+do
+    for nclients in 1 10 20 30 40 50 60 70 80 90 100
+    do
+        echo "Running for $nclients, repeat $repeat..." | tee automate.log
+        python2 genconfig.py $nclients "exp${nclients}_${repeat}"
+        ./run_all.sh
+
+        sleep 3
+        ./killall.sh
+        rm *.conf
+    done
+done
