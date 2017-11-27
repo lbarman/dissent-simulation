6c93b2de2b0e69c7993cb2097a570a77c44dfd77
Ludovic Barman <lbarman@protonmail.ch>
2017-11-27 10:55:32 +0100

    Improves send_data

--- send_data.sh
+++ send_data.sh
@@ -7,7 +7,7 @@ while true
 do
     data="data$i"
     echo -en "Sending $data\t\t"
-    curl -s --data "$data" http://localhost:8080/session/send 1>/dev/null 2>&1
+    curl -s --data "$data" http://localhost:8080/session/send\?data=$1 1>/dev/null 2>&1
     curl -s http://localhost:8080/session/messages\?offset\=0\&count\=999999999999
     echo ""
     sleep 1
