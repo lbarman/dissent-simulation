37c40238c079472b735ce3a5b1a5ec2fc0cb2a94
Ludovic Barman <lbarman@protonmail.ch>
2017-11-27 10:05:01 +0100

    New automate script

--- automate.sh
+++ automate.sh
@@ -2,15 +2,45 @@
 
 rm automate.log
 
+maxRetriesForOneBatch=3
+oneBatchSleepTime=180
+
+# $1 is the filename, e.g. exp1_0_send.log, $2 is the retry number
+function testIfReportingOK {
+    fileName="$1"
+    retries="$2"
+    lastReportingNumber=$(cat "$fileName" | tail -n 1 | cut -d':' -f 4 | cut -d' ' -f 2)
+    if [ $lastReportingNumber -eq 0 ]; then
+        
+        if [ $retries -lt $maxRetriesForOneBatch ]; then
+
+            echo -n "Wait($retries),"
+            sleep "$oneBatchSleepTime"
+            testIfReportingOK "$fileName" $(($retries+1))
+
+        else 
+
+            echo "Failed."
+
+        fi
+
+    else
+        echo "Success".
+    fi
+}
+
 for repeat in 0 1 2 3 4 5 6 7 8 9
 do
     for nclients in 1 10 20 30 40 50 60 70 80 90 100
     do
-        echo "Running for $nclients, repeat $repeat..." | tee automate.log
+        echo -n "Running for $nclients, repeat $repeat..." | tee automate.log
         python2 genconfig.py $nclients "exp${nclients}_${repeat}"
         ./run_all.sh
 
-        sleep 180
+        sleep "$oneBatchSleepTime"
+
+        testIfReportingOK "exp${nclients}_${repeat}_send.log" 1
+
         ./killall.sh
         rm *.conf
     done
